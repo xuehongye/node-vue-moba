0f26dd8e7d90b6099ede73b5bcbfc701b8d41315
xuehongyan <xuehy@qihuangtcm.com>
2020-12-16 09:35:23 +0800

    生产环境编译

--- admin/src/http.js
+++ admin/src/http.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import router from './router'
 
 const http = axios.create({
+    // 必须以VUE_APP开头
     baseURL: process.env.VUE_APP_API_URL || '/admin/api',
     // baseURL: 'http://localhost:3000/admin/api'
 })
